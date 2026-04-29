0a0c323c3b839d720d1a2fd1a1019045b2f2ec3c
Yashwant Kumar <sahratan144@gmail.com>
2026-04-29 10:23:58 +0530

    checkpoint - Interface and greeting

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string userName;
+    int uId, input;
+    //Interface for EVM Machine
+    system("cls");
+    cout << "[][][][][][][][][][]" << endl;
+    cout << "[]                []" << endl;
+    cout << "[]   EVM Machine  []" << endl;
+    cout << "[]                []" << endl;
+    cout << "[][][][][][][][][][]" << endl;
+    //Greeting from machine
+    cout << "\nWelcome to EVM Machine" << endl;
+    cout << "" << endl;
+
+    return 0;
+}
